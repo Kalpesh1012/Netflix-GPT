31e868ee720ac7ca636b5b1f1ac3e053cd1c232c
Kalpesh Mahale <67682474+Kalpesh1012@users.noreply.github.com>
2023-11-28 15:21:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Body from "./component/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appstore";
+
+function App() {
+  return (
+    <>
+      <Provider store={appStore}>
+        <Body />
+      </Provider>
+    </>
+  );
+}
+
+export default App;
